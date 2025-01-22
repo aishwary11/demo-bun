382aa449c234bbddba3bd0e437a97330adb6d908
aishwary11 <aishwary46@gmail.com>
2025-01-22 21:09:28 +0530

    bun with gemini

---
+++ src/middleware/logger.ts
@@ -0,0 +1,23 @@
+import type { NextFunction, Request, Response } from 'express';
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  const start = Date.now();
+  res.on('finish', () => {
+    const latency = Date.now() - start;
+    const formattedTime = new Intl.DateTimeFormat('hi-IN', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: true,
+    }).format(new Date());
+    console.log(
+      `Time: ${formattedTime}, Method: ${req.method}, originalUrl: ${req.originalUrl}, params: ${JSON.stringify(req.params)}, body: ${JSON.stringify(req.body)}, Latency: ${latency} ms`,
+    );
+  });
+  next();
+};
+
+export default logger;
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import cors from "cors";
+import "dotenv/config";
+import type { Express, Request, Response } from "express";
+import express from "express";
+import helmet from "helmet";
+import pc from "picocolors";
+import logger from "./middleware/logger";
+const app: Express = express();
+const port: number = 5000;
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(logger);
+app.post('/', async (req: Request, res: Response) => {
+  const { prompt } = req.body;
+  const { response: { text } } = await model.generateContent([prompt]);
+  res.status(200).json({ msg: "hello", result: text() });
+});
+
+app.listen(port, () => console.log(`Server Started at port: ${pc.yellow(port)}`));
